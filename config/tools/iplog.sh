88c5e736dd2f89d8f56d17d295ceb59cc1bc69ca
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-05-19 13:47:11 +0900

    clean

--- config/tools/iplog.sh
+++
@@ -1,2 +0,0 @@
-#sudo cat /var/log/nginx/access.log | grep "hls" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq > ~/ips.txt
-sudo cat /var/log/nginx/yi.log | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort | uniq > ~/ips.txt
